52e1af59697b9eae9e581fde508e9458f71250d0
Christian Cain <christian22cain@yahoo.com>
2020-12-14 15:16:49 -0600

    created flatiron file tree in desktop

---
+++ flatiron_sweep/generate_file_tree.py
@@ -0,0 +1,31 @@
+# Generates file tree in Desktop directory
+# Note - Designed for Mac!
+
+# - Flatiron
+# 	- ruby
+# 	- sinatra
+# 	- rails
+# 	- javascript
+# 	- react
+# 	- misc
+
+import os
+
+def generate_file_tree():
+	print('Creating file tree...')
+
+	desktop = os.path.expanduser('~/desktop')
+	parent_path = f'{desktop}/flatiron'
+
+	try:
+	    os.mkdir(parent_path)
+	    os.mkdir(f'{parent_path}/ruby')
+	    os.mkdir(f'{parent_path}/sinatra')
+	    os.mkdir(f'{parent_path}/rails')
+	    os.mkdir(f'{parent_path}/javascript')
+	    os.mkdir(f'{parent_path}/react')
+	    os.mkdir(f'{parent_path}/misc')
+	except OSError:
+	    print ("Creation of the directory %s failed" % parent_path)
+	else:
+	    print ("Successfully created the directory %s " % parent_path)
