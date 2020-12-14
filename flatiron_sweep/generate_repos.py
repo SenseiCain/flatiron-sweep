0c2a65b37a253da666c79f804b5830a356e6f219
Christian Cain <christian22cain@yahoo.com>
2020-12-14 15:07:16 -0600

    pulls list of repos & conditionally creates file

---
+++ flatiron_sweep/generate_repos.py
@@ -0,0 +1,26 @@
+from github import Github
+import os
+
+# Generates a list of repos that are to later be cloned &  deleted
+def generate_repos(regex_exp, github_user, github_token, create_txt = False):
+	print('Fetching list of repos...')
+
+	# Create GitHub instance
+	g = Github(github_user, github_token)
+	user = g.get_user()
+	user.login
+
+	list_of_repos = []
+
+	# Conditionally create file
+	if create_txt:
+		desktop = os.path.expanduser('~/desktop')
+		repo_file = open(desktop + '/flation_sweep_repos.txt', 'w')
+		
+		for repo in g.get_user().get_repos():
+			if regex_exp in repo.name:
+				list_of_repos.append(repo.name)
+				repo_file.write(repo.name)
+				repo_file.write('\n')
+
+	return list_of_repos
