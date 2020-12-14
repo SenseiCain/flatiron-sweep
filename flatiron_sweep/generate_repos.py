263b1ed90bea0dc2491753f4cf116189525fd0bb
Christian Cain <christian22cain@yahoo.com>
2020-12-14 15:27:12 -0600

    reorderd conditional

--- flatiron_sweep/generate_repos.py
+++ flatiron_sweep/generate_repos.py
@@ -12,15 +12,18 @@ def generate_repos(regex_exp, github_user, github_token, create_txt = False):
 
 	list_of_repos = []
 
+	# Fetches repos that match regex
+	for repo in g.get_user().get_repos():
+		if regex_exp in repo.name:
+			list_of_repos.append(repo.name)
+
 	# Conditionally create file
 	if create_txt:
 		desktop = os.path.expanduser('~/desktop')
 		repo_file = open(desktop + '/flation_sweep_repos.txt', 'w')
 		
-		for repo in g.get_user().get_repos():
-			if regex_exp in repo.name:
-				list_of_repos.append(repo.name)
-				repo_file.write(repo.name)
-				repo_file.write('\n')
+		for repo_name in list_of_repos:
+			repo_file.write(repo_name)
+			repo_file.write('\n')
 
 	return list_of_repos
