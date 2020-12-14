b4589745de21a6f2323a3f62f451cc7d8af3ea21
Christian Cain <christian22cain@yahoo.com>
2020-12-14 15:07:29 -0600

    tests for --txt flag

--- flatiron_sweep/__main__.py
+++ flatiron_sweep/__main__.py
@@ -1,11 +1,22 @@
 import sys
+from .generate_repos import *
 
 def main():
-    print('in main')
-    args = sys.argv[1:]
-    print('count of args :: {}'.format(len(args)))
-    for arg in args:
-        print('passed argument :: {}'.format(arg))
+	args = sys.argv[1:]
+	regex_exp = '081219'
+	create_text = False
+
+	# Credentials
+	with open('credentials.txt', 'r') as f:
+		token_array = f.read().split('\n')
+		github_user = token_array[0]
+		github_token = token_array[1]
+
+	if '--txt' in args:
+		# Create repo.txt if --txt flag present
+		create_text = True
+
+	generate_repos(regex_exp, github_user, github_token, create_text)
 
 if __name__ == '__main__':
     main()
