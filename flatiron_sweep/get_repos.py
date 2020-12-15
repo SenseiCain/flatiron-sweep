64656bae26bcc75b5b2cafd5b9472333a68e0550
Christian Cain <christian22cain@yahoo.com>
2020-12-15 15:02:33 -0600

    broke fetching & file creation into two functions

--- flatiron_sweep/get_repos.py
+++ flatiron_sweep/get_repos.py
@@ -1,8 +1,7 @@
 from github import Github
-import os
 
 # Generates a list of repos that are to later be cloned &  deleted
-def generate_repos(regex_exp, github_user, github_token, create_txt = False):
+def get_repos(regex_exp, github_user, github_token):
 	print('Fetching list of repos...')
 
 	# Create GitHub instance
@@ -16,14 +15,5 @@ def generate_repos(regex_exp, github_user, github_token, create_txt = False):
 	for repo in g.get_user().get_repos():
 		if regex_exp in repo.name:
 			list_of_repos.append(repo.name)
-
-	# Conditionally create file
-	if create_txt:
-		desktop = os.path.expanduser('~/desktop')
-		repo_file = open(desktop + '/flation_sweep_repos.txt', 'w')
-		
-		for repo_name in list_of_repos:
-			repo_file.write(repo_name)
-			repo_file.write('\n')
 
 	return list_of_repos
