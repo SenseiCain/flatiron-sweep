263d2675b590bf6433029baeea83812552449d02
Christian Cain <christian22cain@yahoo.com>
2020-12-15 15:03:42 -0600

    implemented argparse

--- flatiron_sweep/__main__.py
+++ flatiron_sweep/__main__.py
@@ -1,45 +1,46 @@
 import sys
-from .generate_repos import *
+import argparse
 from .generate_file_tree import *
-from .clone_repo import *
+from .get_repos import *
+from .generate_list_file import *
+
+regex_exp = '081219'
+
+parser = argparse.ArgumentParser(description='Clones & deletes Flatiron repos')
+
+# Txt flag creates a file with a list of flagged repos
+parser.add_argument('--txt', action="store_true",
+	help='Generates a txt file listing flagged repos')
+
+# Clone flag creates clones all repos that meet condition
+parser.add_argument('--clone', action="store_true",
+	help='Create directory in Dekstop & clones all flagged repos')
+
+# Delete flag creates deletes all repos that meet condition
+parser.add_argument('--delete', action="store_true",
+	help='Deletes all flagged repos')
+
+args = parser.parse_args()
+
+def gather_credentials():
+	github_user = input('User: ')
+	github_token = input('Token: ')
+	return [github_user, github_token]
 
 def main():
-	args = sys.argv[1:]
-	regex_exp = '081219'
-	create_text = False
+	if args.txt or args.clone or args.delete:
+		credentials = gather_credentials()
+		github_user = credentials[0]
+		github_token = credentials[1]
 
-	# Credentials
-	with open('credentials.txt', 'r') as f:
-		token_array = f.read().split('\n')
-		github_user = token_array[0]
-		github_token = token_array[1]
+		list_of_repos = get_repos(regex_exp, github_user, github_token)
 
-	# Displays instruction
-	if '--help' in args:
-		print('CLI for cloning & deleting Flatiron repos')
-		print('Requires 3 args: GitHub Username, GitHub access key, and cohort date')
-		print('Caution! Run with --txt flag to see all flagged repos prior to deleting!')
-		print('\n')
-		print('[--txt]      generates a txt file listing all flagged repos')
-		print('[--clone]    generates a folder in desktop & clones all repos')
-		print('[--delete]   deletes all repos that match')
-	else:
-		# Create repo.txt if --txt flag present
-		if '--txt' in args:
-			create_text = True
-
-		list_of_repos = generate_repos(regex_exp, github_user, github_token, create_text)
-
-		if '--clone' in args:
-			# Create directory in Desktop
-			desktop = os.path.expanduser('~/desktop')
-			parent_path = f'{desktop}/flatiron'
-			generate_file_tree(parent_path)
-
-			# Clones repos that match regex
-			# Cloned to file tree created above w/ basic filtering
-			for repo in list_of_repos[:5]:
-				clone_repo(repo, parent_path, github_user)
+		if args.txt:
+			generate_list_file(list_of_repos)
+		if args.clone:
+			print('--clone flag used')
+		elif args.delete:
+			print('--delete flag used')
 
 
 if __name__ == '__main__':
