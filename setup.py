08b03d269b51159590ded064513bef1179e19f7a
Christian Cain <christian22cain@yahoo.com>
2020-12-16 12:51:24 -0600

    updated version for rebuild

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name = 'flatiron_sweep',
     author = 'SenseiCain',
     url = 'https://github.com/SenseiCain/flatiron_sweep',
-    version = '0.1.0',
+    version = '0.1.1',
     packages = ['flatiron_sweep'],
     install_requires=[
     	'PyGithub',
