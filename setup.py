733930aea104e2239d646f319d59061a264451df
Christian Cain <christian22cain@yahoo.com>
2020-12-16 10:25:02 -0600

    added dependencies

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ setup(
     name = 'flation_sweep',
     version = '0.1.0',
     packages = ['flatiron_sweep'],
+    install_requires=[
+    	'PyGithub',
+    	'gitpython'],
     entry_points = {
         'console_scripts': [
             'flatiron_sweep = flatiron_sweep.__main__:main'
